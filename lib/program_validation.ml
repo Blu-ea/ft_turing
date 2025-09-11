8f3baca6e045558f716b45eb8e16abb26ddc6016
Isioblast <th.dv74@gmail.com>
2025-09-11 20:18:34 +0200

    add validation function for alphabet and states

---
+++ lib/program_validation.ml
@@ -0,0 +1,45 @@
+(*
+    Check if alphabet is a list of strings, each one of size 1
+    Return Ok if true, Error with message if false
+*)
+let rec check_alphabet_format (alphabet : string list) = 
+    match alphabet with
+    | [] -> Ok true
+    | head::tail ->
+        if String.length head = 1 then
+            check_alphabet_format tail
+        else
+            Error ("Alphabet contains invalid symbol: " ^ head)
+
+
+(* 
+    Check if a string list contain duplicate
+    Return true if duplicates are found, false otherwise
+*)
+let rec has_duplicates (str_list : string list) =
+    match str_list with
+    | [] -> false
+    | head::tail ->
+        if List.mem head tail then
+            true
+        else
+            has_duplicates tail
+
+
+(*
+    Check if a string is in a list of strings
+    Return true if the string is present in the list, false otherwise
+*)
+let rec string_in_list (s : string) (str_list : string list) =
+    match str_list with
+    | [] -> false
+    | head::tail -> if head = s then true else string_in_list s tail
+
+(*
+    Check if all elements of a list are present in a second list
+    Return true if all elements are present of list1 are present in list2, false otherwise
+*)
+let rec list_in_list (list1 : string list) (list2 : string list) =
+    match list1 with
+    | [] -> true
+    | head::tail -> if string_in_list head list2 then list_in_list tail list2 else false
