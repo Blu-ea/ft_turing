dc37fa7ab46008c2391244016529d91f3222639e
Blue <82770859+Blu-ea@users.noreply.github.com>
2025-09-14 19:05:57 +0200

    update : check for empty name

--- bin/validate_program.ml
+++ bin/validate_program.ml
@@ -6,13 +6,15 @@ open Program_type
     Return Ok if the program is valid, Error with message otherwise
 *)
 let validate_program (program : Program.t) =
-    match validate_alphabet program.alphabet with
-    | Error msg -> Error msg
-    | Ok () ->
-        match List.mem program.blank program.alphabet with
-        | false -> Error ("Blank symbol " ^ program.blank ^ " is not in the alphabet")
-        | true ->
-            match check_states_names program.states program.initial program.finals with
-            | Error msg -> Error msg
-            | Ok () ->
-                check_program_transitions program.transitions program.states program.alphabet
+    if String.length program.name == 0 then Error "The program name is empty"
+    else
+        match validate_alphabet program.alphabet with
+        | Error msg -> Error msg
+        | Ok () ->
+            match List.mem program.blank program.alphabet with
+            | false -> Error ("Blank symbol " ^ program.blank ^ " is not in the alphabet")
+            | true ->
+                match check_states_names program.states program.initial program.finals with
+                | Error msg -> Error msg
+                | Ok () ->
+                    check_program_transitions program.transitions program.states program.alphabet
